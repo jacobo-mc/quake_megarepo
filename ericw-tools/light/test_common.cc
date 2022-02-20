2f598a9c3323cb0e7f1ddca93a3e84c00d455c92
Jacobo <77410525+jacobo-mc@users.noreply.github.com>
2022-02-19 20:04:19 -0500

    Delete ericw-tools directory

--- ericw-tools/light/test_common.cc
+++
@@ -1,9 +0,0 @@
-#include "gtest/gtest.h"
-
-#include <string>
-#include <common/cmdlib.hh>
-
-TEST(common, StripFilename) {
-    ASSERT_EQ("/home/foo", StrippedFilename("/home/foo/bar.txt"));
-    ASSERT_EQ("", StrippedFilename("bar.txt"));
-}
